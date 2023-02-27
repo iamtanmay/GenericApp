4c9dc52c8457e7ca0a66aad9621ed70bfa49ca11
iamtanmay <iamtanmay@gmail.com>
2023-02-27 08:03:35 +0100

    Update Objectives.cs

--- Scripts/Journal/Objectives.cs
+++ Scripts/Journal/Objectives.cs
@@ -2,130 +2,132 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using AppStarter;
 
-public class Objectives
+namespace AppStarter
 {
-    public List<string> flagNames = new List<string>();
-    public List<bool> flagValues = new List<bool>();
-    public GameObject quest;
-    public string questID;
-    public int objectiveCounter = 0;
-    public int currObjective = 0;
-    //Can objectives be succeeded in linear order only or not
-    public bool areObjectivesLinear = false;
-    public API API;
-
-    public Objectives()
+    public class Objectives
     {
-        API[] temp = Object.FindObjectsOfType<API>();
-        API = temp[0];
-    }
+        public List<string> flagNames = new List<string>();
+        public List<bool> flagValues = new List<bool>();
+        public GameObject quest;
+        public string questID;
+        public int objectiveCounter = 0;
+        public int currObjective = 0;
+        //Can objectives be succeeded in linear order only or not
+        public bool areObjectivesLinear = false;
+        public API API;
 
-    public void Initialise(GameObject iquest, bool linear, List<string> iflags)
-    {
-        quest = iquest;
-        areObjectivesLinear = linear;
-        Debug.Log("Quest is linear: " + areObjectivesLinear);
-        CreateFlags(iflags);
-    }
-
-    public bool ExistsFlag(string iKey)
-    {
-        return flagNames.Contains(iKey);
-    }
-
-    public bool GetFlag(string iKey)
-    {
-        if (ExistsFlag(iKey))
+        public Objectives()
         {
-            int index = flagNames.FindIndex(a => a == iKey);
-            return flagValues[index];
+            API[] temp = Object.FindObjectsOfType<API>();
+            API = temp[0];
         }
-        return false;
-    }
 
-    public void SetFlag(string iKey, bool iflag)
-    {
-        if (ExistsFlag(iKey))
+        public void Initialise(GameObject iquest, bool linear, List<string> iflags)
         {
-            int index = flagNames.FindIndex(a => a==iKey);
-            SetFlag(index, iflag);
+            quest = iquest;
+            areObjectivesLinear = linear;
+            Debug.Log("Quest is linear: " + areObjectivesLinear);
+            CreateFlags(iflags);
         }
-    }
 
-    public void SetFlag(int index, bool iflag)
-    {
-        Debug.Log("Setting Flag " + index + " to " + iflag);
-        flagValues[index] = iflag;
-        CustomEvent.Trigger(quest, "updateJournal", index, iflag);
-    }
-
-    public void CreateFlags(List<string> iflags)
-    {
-        flagNames = iflags;
-        flagValues = new List<bool>();
-
-        for (int i = 0; i < iflags.Count; i++)
-            flagValues.Add(false);
-    }
-
-    public void CountSuccessFlags()
-    {
-        objectiveCounter = 0;
-
-        for (int i = 0; i < flagValues.Count; i++)
-            if (flagValues[i])
-                objectiveCounter++;
-    }
-
-    public bool UpdateObjectives(string ievent, bool iflag, string iID)
-    {
-        Debug.Log("Event Triggered " + ievent + " " + iflag + " " + iID);
-        if (flagNames.Contains(ievent))
+        public bool ExistsFlag(string iKey)
         {
-            //False flag can always be set
-            if (!iflag)
+            return flagNames.Contains(iKey);
+        }
+
+        public bool GetFlag(string iKey)
+        {
+            if (ExistsFlag(iKey))
             {
-                SetFlag(ievent, iflag);
-                CountSuccessFlags();
-                return false;
+                int index = flagNames.FindIndex(a => a == iKey);
+                return flagValues[index];
             }
-            else
+            return false;
+        }
+
+        public void SetFlag(string iKey, bool iflag)
+        {
+            if (ExistsFlag(iKey))
             {
-                if (areObjectivesLinear)
+                int index = flagNames.FindIndex(a => a == iKey);
+                SetFlag(index, iflag);
+            }
+        }
+
+        public void SetFlag(int index, bool iflag)
+        {
+            Debug.Log("Setting Flag " + index + " to " + iflag);
+            flagValues[index] = iflag;
+            CustomEvent.Trigger(quest, "updateJournal", index, iflag);
+        }
+
+        public void CreateFlags(List<string> iflags)
+        {
+            flagNames = iflags;
+            flagValues = new List<bool>();
+
+            for (int i = 0; i < iflags.Count; i++)
+                flagValues.Add(false);
+        }
+
+        public void CountSuccessFlags()
+        {
+            objectiveCounter = 0;
+
+            for (int i = 0; i < flagValues.Count; i++)
+                if (flagValues[i])
+                    objectiveCounter++;
+        }
+
+        public bool UpdateObjectives(string ievent, bool iflag, string iID)
+        {
+            Debug.Log("Event Triggered " + ievent + " " + iflag + " " + iID);
+            if (flagNames.Contains(ievent))
+            {
+                //False flag can always be set
+                if (!iflag)
                 {
-                    //Check to see if previous flags are true
-                    int index = flagNames.FindIndex(a => a == ievent);                    
-
-                    for (int i = 0; i < index; i++)
-                        if (!flagValues[i])
-                        {
-                            //Set subsequent flags to false as well
-                            for (int j = i+1; j <= index; j++)
-                                SetFlag(j, false);
-                            return false;
-                        }
-
-                    SetFlag(index, iflag);
-                    Debug.Log(index + " " + iflag + " " + currObjective);
-                    if (iflag == true && index >= currObjective)
-                    {
-                        currObjective++;
-                        API.updateCurrentPriority(currObjective);
-                    }
+                    SetFlag(ievent, iflag);
+                    CountSuccessFlags();
+                    return false;
                 }
                 else
                 {
-                    SetFlag(ievent, iflag);
+                    if (areObjectivesLinear)
+                    {
+                        //Check to see if previous flags are true
+                        int index = flagNames.FindIndex(a => a == ievent);
+
+                        for (int i = 0; i < index; i++)
+                            if (!flagValues[i])
+                            {
+                                //Set subsequent flags to false as well
+                                for (int j = i + 1; j <= index; j++)
+                                    SetFlag(j, false);
+                                return false;
+                            }
+
+                        SetFlag(index, iflag);
+                        Debug.Log(index + " " + iflag + " " + currObjective);
+                        if (iflag == true && index >= currObjective)
+                        {
+                            currObjective++;
+                            API.updateCurrentPriority(currObjective);
+                        }
+                    }
+                    else
+                    {
+                        SetFlag(ievent, iflag);
+                    }
+
+                    CountSuccessFlags();
+
+                    if (objectiveCounter == flagValues.Count)
+                        return true;
                 }
-
-                CountSuccessFlags();
-
-                if (objectiveCounter == flagValues.Count)
-                    return true;
             }
+            return false;
         }
-        return false;
     }
 }
