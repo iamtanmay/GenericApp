c41f27cd6070d2271498cb2733adea5f51983394
iamtanmay <iamtanmay@gmail.com>
2023-02-27 05:58:00 +0100

    Delete Scripts directory

--- Scripts/Behaviors/BehaviorBase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Behavior : MonoBehaviour
-{
-    public List<string> strings;
-    public Dictionary<string, string> commands;
-}
--- Scripts/KeepTransform.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class KeepTransform : MonoBehaviour{
-
-    void Update(){
-        transform.localPosition = Vector3.zero;
-        transform.localRotation = Quaternion.identity;
-    }
-}
