3ede580ac4ba071c2eedcad22f7ca36b38ff42fc
iamtanmay <iamtanmay@gmail.com>
2023-03-01 20:09:42 +0100

    Update 0backupScripts.bat

--- 0backupScripts.bat
+++ 0backupScripts.bat
@@ -2,7 +2,7 @@ mkdir ScriptsBckup
 robocopy ./Scripts ./ScriptsBckup/Scripts /E
 cd ScriptsBckup
 del /s *.meta
-powershell.exe Compress-Archive ./Scripts ../Scripts_%date:~7,2%-%date:~4,2%-%date:~10,4%.zip
+powershell.exe Compress-Archive -Force ./Scripts ../Scripts_%date:~7,2%-%date:~4,2%-%date:~10,4%.zip
 cd ..
 rmdir /s /q ScriptsBckup
 git add Scripts/\\*
